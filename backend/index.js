c1ee61446bb408be0c9d445494b747cc26ce58d9
Chandan Thakur <contact.chandanthakur@gmail.com>
2025-08-17 10:33:00 +0530

    hosting

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const authMiddleware = require('./middleware/auth.middleware');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.BASE_URL,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
 app.use(express.json());
 
 // PostgreSQL connection
